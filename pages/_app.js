99b4a29a213e0c60f16a0720204aedfec83377cf
Philihp Busby <philihp@gmail.com>
2020-11-12 09:31:44 +0000

    create pages for login

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import Router from 'next/router'
+import { pageview } from '../lib/gtag'
 import '../styles/globals.css'
+
+Router.events.on('routeChangeComplete', pageview)
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />

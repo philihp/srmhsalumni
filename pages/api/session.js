e4d0876b38a7300994da21a22ee79d83fe5ce7e5
Philihp Busby <philihp@gmail.com>
2020-11-13 12:11:15 +0000

    update auth0 for preview urls

--- pages/api/session.js
+++ pages/api/session.js
@@ -2,7 +2,7 @@ import auth0 from '../../lib/auth0'
 
 export default async function session(req, res) {
   try {
-    const tokenCache = auth0.tokenCache(req, res)
+    const tokenCache = auth0(req).tokenCache(req, res)
     const { accessToken } = await tokenCache.getAccessToken()
     res.status(200).json({ accessToken })
   } catch (error) {

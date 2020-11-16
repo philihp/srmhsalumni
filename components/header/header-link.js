354303b83275d42db7f9d47df5df281fc7be27de
Philihp Busby <philihp@gmail.com>
2020-11-16 14:40:27 +0000

    stylize links

--- components/header/header-link.js
+++ components/header/header-link.js
@@ -4,7 +4,9 @@ import Link from 'next/link'
 const HeaderLink = ({ href, children }) => (
   <li className="px-2 md:px-4">
     <Link href={href}>
-      <a>{children}</a>
+      <a className="text-gray-500 font-semibold hover:text-purple-500">
+        {children}
+      </a>
     </Link>
   </li>
 )

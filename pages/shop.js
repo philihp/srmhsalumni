b322dbde2a320adab7f32196d145e75885e46459
Philihp Busby <philihp@gmail.com>
2020-11-28 22:34:30 +0000

    Update shop.js

--- pages/shop.js
+++ pages/shop.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Shop = () => (
   <div>
-    We do not have a shop yet--- we will be creating one… so for now this can be
+    We do not have a shop yet&emdash; we will be creating one… so for now this can be
     wait-listed, or a place holder can be put up
   </div>
 )

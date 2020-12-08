b848d8b42e41d07f5f0917108bbc387589009866
ClarissaJones <74931384+ClarissaJones@users.noreply.github.com>
2020-12-08 09:53:06 -0500

    Update contact-us.js

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ContactUs = () => (
   <div>
     For questions or comments, please contact the association at
-    <a href="mailto:srmhsalumni@gmail.com">srmhsalumni@gmail.com</a>
+     <a href="mailto:srmhsalumni@gmail.com">srmhsalumni@gmail.com</a>
   </div>
 )
 

255233563e62cbc8a47e39598d2f6a4da164974d
ClarissaJones <74931384+ClarissaJones@users.noreply.github.com>
2020-12-08 09:39:01 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -51,12 +51,7 @@ const Index = () => {
       <p>With warmest regard,</p>
       <p>Tylisa Finley</p>
       <p>President, SRMHS Alumni Association</p>
-      <Image
-        src="/SESchoolPhoto.jpg"
-        alt="Southeast Raleigh High School"
-        width={1396}
-        height={786}
-      />
+      
     </>
   )
 }

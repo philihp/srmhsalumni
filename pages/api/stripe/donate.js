5e234a0101059a0a352fb296bd0f99b4144d34d9
Philihp Busby <philihp@gmail.com>
2020-12-13 07:42:42 +0000

    example of custom amount to donate

---
+++ pages/api/stripe/donate.js
@@ -0,0 +1,36 @@
+import stripe from '../../../lib/stripe'
+
+export default async function donate(req, res) {
+  if (req.method === 'POST') {
+    const { customer_email: customerEmail, amount } = req.body
+    const ses = await stripe.checkout.sessions.create({
+      customer_email: customerEmail,
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price_data: {
+            unit_amount: amount,
+            currency: 'usd',
+            product: 'prod_IYwKqBSozmTMFr',
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${req.headers.origin}/api/stripe/success?sessionId={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${req.headers.origin}/membership`,
+    })
+    return res.status(200).json({ sessionId: ses.id })
+  }
+
+  if (req.method === 'GET') {
+    const { sessionId } = req.query
+    if (sessionId === undefined)
+      return res.status(400).end('GET requires a sessionID param')
+    const checkoutSession = await stripe.checkout.sessions.retrieve(sessionId)
+    return res.status(200).json(checkoutSession)
+  }
+
+  res.setHeader('Allow', 'POST', 'GET')
+  return res.status(405).end('Method Not Allowed')
+}
--- pages/donate.js
+++ pages/donate.js
@@ -1,46 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { loadStripe } from '@stripe/stripe-js'
+import { withApollo } from '../lib/withApollo'
+import { useFetchUser } from '../lib/user'
 
-const Donate = () => (
-  <div>
-    <h2>Donate</h2>
-    <form method="GET" action="#">
-      <label>
-        <div className="mt-4">Name</div>
-        <input
-          className="form-input border rounded shadow-md"
-          id="name"
-          type="text"
-          name="name"
-        />
-      </label>
-      <label>
-        <div className="mt-4">Email</div>
-        <input
-          className="form-input border rounded shadow-md"
-          id="email"
-          type="text"
-          name="email"
-        />
-      </label>
-      <label>
-        <div className="mt-4">Amount</div>
-        <input
-          className="form-input border rounded shadow-md"
-          id="amount"
-          type="number"
-          name="amount"
-        />
-      </label>
-      <div className="mt-8">
-        <button
-          className="form-button form-input bg-indigo-600 hover:bg-indigo-700 text-white justify-center w-1/3 cursor-pointer rounded shadow-md"
-          type="submit"
-        >
-          Donate
-        </button>
-      </div>
-    </form>
+const stripePromise = loadStripe(process.env.STRIPE_PUBLIC)
+
+const gotoPayment = async ({ email }) => {
+  const body = JSON.stringify({
+    amount: 1234,
+    customer_email: email,
+  })
+  const { sessionId } = await fetch('/api/stripe/donate', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body,
+  }).then((res) => res.json())
+  console.log({ sessionId })
+  const stripe = await stripePromise
+  console.log({ sessionId })
+  const { error } = await stripe.redirectToCheckout({ sessionId })
+  if (error) console.error(error)
+}
+
+const LoadingUser = () => (
+  <div className="bg-blue-100 shadow-md rounded p-4 flex flex-col">
+    Loading form...
   </div>
 )
 
-export default Donate
+const Donate = () => {
+  const { user, loading: userLoading } = useFetchUser({ required: true })
+  return (
+    <div>
+      <h2>Donate</h2>
+      {userLoading && <LoadingUser />}
+      {!userLoading && (
+        <form
+          action="#"
+          method="POST"
+          className="bg-blue-100 shadow-xl rounded p-4 flex flex-col"
+          onSubmit={(e) => {
+            e.preventDefault()
+            gotoPayment({ email: user.name })
+          }}
+        >
+          <label>
+            <div className="mt-4">Name</div>
+            <input
+              className="form-input border rounded shadow-md"
+              id="name"
+              type="text"
+              name="name"
+            />
+          </label>
+          <label>
+            <div className="mt-4">Email</div>
+            <input
+              className="form-input border rounded shadow-md"
+              id="email"
+              type="text"
+              name="email"
+            />
+          </label>
+          <label>
+            <div className="mt-4">Amount</div>
+            <input
+              className="form-input border rounded shadow-md"
+              id="amount"
+              type="number"
+              name="amount"
+            />
+          </label>
+          <div className="mt-8">
+            <button
+              className="form-button form-input bg-indigo-600 hover:bg-indigo-700 text-white justify-center w-1/3 cursor-pointer rounded shadow-md"
+              type="submit"
+            >
+              Donate
+            </button>
+          </div>
+        </form>
+      )}
+    </div>
+  )
+}
+
+export default withApollo()(Donate)

7c6f557e5ca864b6d60e81d4497b52439529378a
ClarissaJones <74931384+ClarissaJones@users.noreply.github.com>
2020-12-13 21:59:21 -0500

    Update support.js

--- pages/support.js
+++ pages/support.js
@@ -10,12 +10,7 @@ const Support = () => (
     <p>
       <a href="https://www.gofundme.com/f/p43r2-a-cause-i-care-about-needs-help?member=6361078&amp;utm_source=twilio&amp;utm_medium=sms&amp;utm_campaign=contacts-v2-invite-to-donate&amp;fbclid=IwAR3SFwg-9upT8out_SfltGuxkyQaEtgKTWjD532v8UfS358kUf9ttF3nYjI">
         Support SRHMS Football
-      </a>
-    <p>
-     You can also support the SRMHS Football team and the school by purchases gear! 
-     <a href= "https://www.bsnteamsports.com/shop/DAWG20FB>
-       SRMHS Football Gear
-     </a>   
+         </a>   
   </div>
 )
 

ef3b4efef1fb8f29c105caaec1ec85929c28ac1c
ClarissaJones <74931384+ClarissaJones@users.noreply.github.com>
2020-12-13 21:51:57 -0500

    Update support.js

--- pages/support.js
+++ pages/support.js
@@ -16,6 +16,7 @@ const Support = () => (
      You can also support the SRMHS Football team and the school by purchases gear! 
      <a href= "https://www.bsnteamsports.com/shop/DAWG20FB>
        SRMHS Football Gear
+     </a>   
   </div>
 )
 

f43a610f6c03762e565c10fbf0c02ede8f1f5853
ClarissaJones <74931384+ClarissaJones@users.noreply.github.com>
2020-12-21 12:38:27 -0500

    Update engagement.js

--- pages/engagement.js
+++ pages/engagement.js
@@ -8,7 +8,7 @@ const Engagement = () => (
     <h3>Awards, Scholarships &amp; Nominations</h3>
     <p>
     Chair- Tylisa 
-    <p>
+    </p>
       The Awards, Scholarships, &amp; Nominations Committee will collect and
       review nominations for all designated awards, scholarships, and positions.
       This committee will create the applications for all required awards,

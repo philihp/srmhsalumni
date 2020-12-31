0badcd112f7b32c6385a32fad6e8076c800dd0e1
ClarissaJones <74931384+ClarissaJones@users.noreply.github.com>
2020-12-31 15:22:15 -0500

    Update contact-us.js

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -8,10 +8,15 @@ const ContactUs = () => (
   <p>
   Follow us on social media: 
   </p>
+  <p>
 <a href="www.twitter.com/srmhsalumni">www.twitter.com/srmhsalumni</a>
+  </p>
+  <p>
 <a href="www.instagram.com/srhmhsalumni">www.instagram.com/srmhsalumni</a> 
+  </p>
+  <p>
 <a href="https://www.facebook.com/groups/sralumni/">https://www.facebook.com/groups/sralumni/</a>
-  
+  </p>
   </div>
 )
 

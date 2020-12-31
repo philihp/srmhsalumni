63034068b3157c2dc36ef2c9167ae263bc3fbb0b
ClarissaJones <74931384+ClarissaJones@users.noreply.github.com>
2020-12-31 15:19:08 -0500

    Update contact-us.js

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 const ContactUs = () => (
   <div>
-    For questions or comments, please contact the association at
+    For questions or comments, please contact the association at<p> </p>
     <a href="mailto:srmhsalumni@gmail.com">srmhsalumni@gmail.com</a>
+  
+  <p>
+  Follow us on social media: 
+  </p>
+<a href="www.twitter.com/srmhsalumni">www.twitter.com/srmhsalumni</a>
+<a href="www.instagram.com/srhmhsalumni">www.instagram.com/srmhsalumni</a> 
+<a href="https://www.facebook.com/groups/sralumni/">https://www.facebook.com/groups/sralumni/</a>
+  
   </div>
 )
 
